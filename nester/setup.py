fc7aa8f7fb1b07faccd82538dd38ae72733c0c56
Peisheng YE <Peisheng.ye.88@gmail.com>
2016-10-14 11:37:51 -0400

    python setup modules

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author     =  'peisheng ye',
+        author_email = 'peisheng.ye.88@gamil.com',
+        url           = 'www.radioyps.com',
+        description  =  'A simple printer of nested lists '
+       )
